e3da66881a3122cd51a766e74857004a3840b667
nikitt <nikittaosipov@yandex.ru>
2025-03-22 20:44:21 +0400

    model update

--- tasks/models.py
+++ tasks/models.py
@@ -3,6 +3,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def generate_task_id():
+    return secrets.token_hex(16)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
@@ -11,7 +15,7 @@ class Category(models.Model):
 
 
 class Task(models.Model):
-    id = models.CharField(primary_key=True, default=lambda: secrets.token_hex(16), editable=False, max_length=32)
+    id = models.CharField(primary_key=True, default=generate_task_id, editable=False, max_length=32)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -21,3 +25,12 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    telegram_chat_id = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
+
