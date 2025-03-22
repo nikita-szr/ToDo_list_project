458c61a1c3995757b093ea0064e7ebf759ad4f82
nikitt <nikittaosipov@yandex.ru>
2025-03-22 22:32:05 +0400

    update

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from celery.schedules import crontab
 
 load_dotenv(override=True)
 
