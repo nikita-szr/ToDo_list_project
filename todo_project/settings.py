998a202df569491b2e40c3bcd98cc796588da51c
nikitt <nikittaosipov@yandex.ru>
2025-03-22 18:52:30 +0400

    celery settings

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -135,3 +135,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
