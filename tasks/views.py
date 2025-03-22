dfdc74bee8b7059e520915ad9d407c3a56c870fa
nikitt <nikittaosipov@yandex.ru>
2025-03-22 20:08:44 +0400

    views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task, Category
+from .serializers import TaskSerializer, CategorySerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
