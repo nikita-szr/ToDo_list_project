54057ec7dffa84815daa979ed100c076697b76e4
nikitt <nikittaosipov@yandex.ru>
2025-03-22 18:55:23 +0400

    model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
+import secrets
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    id = models.CharField(primary_key=True, default=lambda: secrets.token_hex(16), editable=False, max_length=32)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
